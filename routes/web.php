0b3f00efed86ff5f36a4cddf873f7cb7a1a9dd9b
João Victor <joao2009victor@hotmail.com>
2025-10-16 21:39:59 -0300

    AULA 41
    Redirecionamento de rotas + Rota de contingência (fallback)

--- routes/web.php
+++ routes/web.php
@@ -7,14 +7,29 @@ Route::get('/', function () {
 });
 */
 
-Route::get('/', [\App\Http\Controllers\Principalcontroller::class, 'principal']);
+Route::get('/', [\App\Http\Controllers\Principalcontroller::class, 'principal'])->name('site.index');
+Route::get('/sobre-nos', [\App\Http\Controllers\SobreNoscontroller::class, 'SobreNos'])->name('site.sobrenos');
+Route::get('/contato', [\App\Http\Controllers\Contatocontroller::class, 'contato'])->name('site.contato');
+Route::get('/login', function(){ return 'Login'; })->name('site.login');
 
-Route::get('/sobre-nos', [\App\Http\Controllers\SobreNoscontroller::class, 'SobreNos']);
+// 3 novas rotas
 
-Route::get('/contato', [\App\Http\Controllers\Contatocontroller::class, 'contato']);
+Route::prefix('/app')->group(function() {
+    Route::get('/clientes', function(){ return 'Clientes'; })->name('app.clientes');
+    Route::get('/fornecedores', function(){ return 'Fornecedores'; })->name('app.fornecedores');
+    Route::get('/produtos', function(){ return 'Produtos'; })->name('app.produtos');
+});
 
-// 4 novas rotas
-Route::get('/login', function(){ return 'Login'; });
-Route::get('/clientes', function(){ return 'Clientes'; });
-Route::get('/fornecedores', function(){ return 'Fornecedores'; });
-Route::get('/produtos', function(){ return 'Produtos'; });
+//FORMAS DE CRIAR UM REDIRECIONAMENTO Route::redirect('/rota2', '/rota1');
+Route::get('/rota1', function() {
+    echo 'Rota 1';
+})->name('site.rota1');
+
+Route::get('/rota2', function() {
+    return redirect()->route('site.rota1');
+})->name('site.rota2');
+
+//ROTA DE CONTINGÊNCIA OU FALLBACK, TEM A FUNÇÃO DE EVITAR A TELA DE ERRO AO USUÁRIO FINAL.
+Route::fallback(function() {
+    echo 'A rota acessada não existe. <a href="'.route('site.index').'">clique aqui<a/>';
+});
