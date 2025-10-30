01aa8c114c997e842338ec1c4b4948a4ef9dfeab
João Victor <joao2009victor@hotmail.com>
2025-10-29 22:36:03 -0300

    @if, @elseif e @endif

--- resources/views/app/fornecedor/index.blade.php
+++ resources/views/app/fornecedor/index.blade.php
@@ -1,1 +1,33 @@
 <h3>Fornecedor</h3>
+
+{{--comentario--}}
+
+{{--para usar php puro deve-se:--}}
+
+@php
+    // Para comentários de uma linha
+    /*
+    Para comentários de múltiplas linhas
+    */
+
+   // echo 'Texto de teste';
+@endphp
+
+@php
+  /*  if() {
+
+    } elseif() {
+
+    } else {
+    
+    }
+    */
+@endphp
+
+@if(count($fornecedores) > 0 && count($fornecedores) < 10)
+    <h3> Existem alguns fornecedores cadastrados </h3>
+@elseif(count($fornecedores) > 10)
+    <h3> Existem vários fornecedores cadastrados </h3>
+@else
+    <h3> Ainda não existem fornecedores cadastrados </h3>
+@endif
