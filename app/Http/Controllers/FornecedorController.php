872c0c15057a2027afbb13980968feb359b2b39a
João Victor <joao2009victor@hotmail.com>
2025-11-06 21:42:13 -0300

    operador condicional ternário

--- app/Http/Controllers/FornecedorController.php
+++ app/Http/Controllers/FornecedorController.php
@@ -10,12 +10,19 @@ class FornecedorController extends Controller
         $fornecedores = [
             0 => ['nome' => 'Fornecedor 1', 
             'status' => 'N', 
-            'cnpj' => 'null'
+            'cnpj' => '147.455.336-24'
         ], 
             1 => ['nome' => 'Fornecedor 2', 
             'status' => 'S', 
             ] 
         ];
+        /*
+        OPERADOR CONDICIONAL TERNÁRIO
+        condicao ? se verdade : se falso;
+        condicao ? se verdade : (condicao ? se verdade : se falso);
+        */
+        $msg = isset($fornecedores[0]['cnpj']) ? 'CNPJ informado' : 'CNPJ não informado';
+        echo $msg;
         return view('app.fornecedor.index', compact('fornecedores'));
     }
 }
